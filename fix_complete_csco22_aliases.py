cf159478e33259256c3464dd64277d8def9224dd
Antao Li <37994491+anthonyliantao@users.noreply.github.com>
2025-03-16 17:00:54 +0800

    create 补充职业别名临时脚本

---
+++ fix_complete_csco22_aliases.py
@@ -0,0 +1,19 @@
+"""临时python脚本 用于修复现在的职业别名库不能覆盖所有职业的问题"""
+
+import pandas as pd
+
+csco_file = 'data/csco22.csv'
+alias_file = 'data/csco22_aliases.csv'
+
+csco_df = pd.read_csv(csco_file)
+alias_df = pd.read_csv(alias_file)
+
+csco_df['alias'] = csco_df['name']
+csco_df.rename(columns={'code': 'csco_code', 'code_num': 'csco_code_num', 'name': 'csco_name'}, inplace=True)
+complete_df = csco_df[['alias', 'csco_code', 'csco_code_num', 'csco_name']]
+
+alias_df = pd.concat([alias_df, complete_df])
+alias_df = alias_df.drop_duplicates()
+alias_df = alias_df.sort_values(by='csco_code_num')
+
+alias_df.to_csv('test.csv', index=False)
