2ee90e9468b00977c83faaca48d9913d88f985c2
Antao Li <37994491+anthonyliantao@users.noreply.github.com>
2025-03-18 11:25:20 +0800

    fix: 修改csco和aliases数据导入方式

--- cscoder/coder.py
+++ cscoder/coder.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
-from .data_loader import load_csco, load_aliases
-from .text_cleaner import clean_job_name
+from .utils import load_csco
+from .preprocess import clean_job_name
 
 
 class CSCOder:
@@ -26,19 +26,29 @@ class CSCOder:
 
     @property
     def csco_data(self):
-        """加载 CSCO 数据"""
+        """加载 CSCO 数据（code -> name）"""
         if self._csco_data is None:
             df = load_csco(self.version)
-            self._csco_data = df.set_index("code_num")["name"].to_dict()
+            self._csco_data = df.set_index("code")["name"].to_dict()
         return self._csco_data
 
     @property
     def alias_data(self):
         """加载职业别名数据"""
         if self._alias_data is None:
-            df = load_aliases(self.version)
-            embeddings = self._encode_texts(df["alias"].tolist(), show_progress_bar=True)
-            self._alias_data = (df, embeddings)
+            df = load_csco(self.version)
+            alias_list = []
+            code_list = []
+
+            for _, row in df.iterrows():
+                for alias in row["alias"]:
+                    alias_list.append(alias)
+                    code_list.append(row["code"])
+
+            alias_df = pd.DataFrame({"alias": alias_list, "code": code_list})
+            embeddings = self._encode_texts(alias_list, show_progress_bar=True)
+
+            self._alias_data = (alias_df, embeddings)
         return self._alias_data
 
     @property
@@ -66,15 +76,15 @@ class CSCOder:
             top_indices = sorted_indices[:top_n]
             for idx in top_indices:
                 sim_score = scores[idx]
-                csco_code = self.alias_df.iloc[idx]["csco_code_num"]
+                csco_code = self.alias_df.iloc[idx]["code"]
                 final_csco_code = self._check_code_for_similarity(
                     csco_code, sim_score)
                 final_csco_name = self.csco_data.get(str(final_csco_code))
                 results.append(
                     {
-                    "csco_code": final_csco_code,
-                    "csco_name": final_csco_name,
-                    "similarity": sim_score
+                        "matched_code": final_csco_code,
+                        "matched_name": final_csco_name,
+                        "similarity": sim_score
                     })
 
         return results
@@ -91,17 +101,17 @@ class CSCOder:
         elif sim_score >= 0.2:
             return csco_code[:3] + "0000"        # 1位代码
         else:
-           return "8000000"                      # 低于 0.2，返回不便分类人员
+            return "8000000"                      # 低于 0.2，返回不便分类人员
 
     def find_best_match(self, job_name, top_n=1, return_df=True):
         """匹配单个职业"""
         if not job_name.strip():
             return []
-        
+
         job_embedding = self._encode_texts([clean_job_name(job_name)])
         match_results = self._hierarchy_match(job_embedding, top_n)
         results = [{"input": job_name, **match} for match in match_results]
-        
+
         return results if not return_df else pd.DataFrame(results)
 
     def find_best_matches(self, job_names, top_n=1, batch_size=1000, return_df=True, show_progress=False):
@@ -117,14 +127,16 @@ class CSCOder:
 
         job_names = list(map(clean_job_name, job_names))
         total_jobs = len(job_names)
-        batches = [job_names[i: i + batch_size] for i in range(0, total_jobs, batch_size)]
+        batches = [job_names[i: i + batch_size]
+                   for i in range(0, total_jobs, batch_size)]
         results = []
 
         with tqdm(total=len(batches), desc="Processing Batches", unit="batch", disable=not show_progress) as pbar:
             for batch in batches:
                 job_embeddings = self._encode_texts(batch)
                 batch_results = self._hierarchy_match(job_embeddings, top_n)
-                results.extend([{"input": input, **match} for input, match in zip(batch, batch_results)])
+                results.extend([{"input": input, **match}
+                               for input, match in zip(batch, batch_results)])
                 pbar.update(1)
 
         return pd.DataFrame(results) if return_df else results
@@ -132,7 +144,7 @@ class CSCOder:
 
 if __name__ == "__main__":
     coder = CSCOder()
-    
+
     # 匹配单个职业
     result = coder.find_best_match("软件工程师")
     print(result)
--- cscoder/utils.py
+++ cscoder/utils.py
@@ -11,18 +11,14 @@ def load_csco(version="csco22"):
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"未找到 {file_path}，请确认 CSV 文件是否存在")
 
-    return pd.read_csv(file_path, usecols=["code_num", "name"], dtype={"code_num": str})
-
-
-def load_aliases(version="csco22"):
-    """加载 CSCO 别名数据"""
-    file_path = os.path.join(DATA_DIR, f"{version}_aliases.csv")
-
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"未找到 {file_path}，请确认 CSV 文件是否存在")
-
-    df = pd.read_csv(file_path).dropna()
-    df["alias"] = df["alias"].str.strip().str.lower()
-    df = df.drop_duplicates(subset=["alias"])
-
+    df = pd.read_csv(file_path, usecols=["code_num", "name", "alias"], dtype=str)
+    
+    df.rename(columns={'code_num': 'code'}, inplace=True)
+    df['alias'] = df["alias"].apply(lambda x: x.split(",") if isinstance(x, str) else [df["name"]])
+    
     return df
+
+
+if __name__ == "__main__":
+    df = load_csco()
+    print(df.head())
